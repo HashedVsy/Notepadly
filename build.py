bd3f7d28d3e06951d346410759fa2c7232e31f0e
Hashed <bilal.karabacak52@icloud.com>
2026-01-18 14:52:06 +0100

    Update build.py

--- build.py
+++ build.py
@@ -4,8 +4,9 @@ import os
 
 # Configuration
 app_name = "Notepadly"
-source_file = "Notepadly.py"
+source_file = "NotepadlyMain.py"
 icon_file = "Notepadly.ico"
 
 # Main
 os.system(f'pyinstaller --noconfirm --onefile --windowed --icon "{icon_file}" --name "{app_name}" --add-data "{icon_file};." "{source_file}"')
+
