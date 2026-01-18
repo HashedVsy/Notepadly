966161cb78465544c349bce653e52fd79fe8c1ef
Hashed <bilal.karabacak52@icloud.com>
2026-01-18 14:51:23 +0100

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
+# Custom Compiler/Wrapper
+
 import os
 
-app_name = "SimplyNotepad"
-source_file = "SimplyNotepad.py"
-icon_file = "SimplyNotepad.ico"
+# Configuration
+app_name = "Notepadly"
+source_file = "Notepadly.py"
+icon_file = "Notepadly.ico"
 
+# Main
 os.system(f'pyinstaller --noconfirm --onefile --windowed --icon "{icon_file}" --name "{app_name}" --add-data "{icon_file};." "{source_file}"')
